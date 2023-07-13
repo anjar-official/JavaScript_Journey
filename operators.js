fbefa87a4b8e871e2e4ed451852bb2bd1fa4daab
Anjar Hussain <101003348+anjar-official@users.noreply.github.com>
2023-07-12 18:55:37 -0700

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,42 @@
+let a = 5;
+let b = 4;
+
+// Arithmetic Operators
+
+console.log("a + b = ", a+b); //addition
+console.log("a - b = ", a-b); // subtraction
+console.log("a * b = ", a*b); //Multiplication
+console.log("a / b = ", a/b); //division
+console.log("a ** b = ", a**b); //exponentiation
+console.log("a % b = ", a%b); // modulus
+console.log("a++ = ", a++); //increment
+console.log("a-- = ", a--); //ddecrememt
+
+
+// Assignment Operators
+let n = 1 ; //assignment 
+
+console.log(n += 5); // n = n + 5
+console.log(n -= 5); // n = n - 5
+console.log(n *= 5); // n = n * 5
+console.log(n /= 5); // n = n / 5
+
+
+// Comparision Operators
+let comp1=6;
+let comp2=6;
+
+console.log(comp1 == comp2);
+console.log(comp1 != comp2);
+console.log(comp1 === comp2); // value and type
+console.log(comp1 !== comp2);
+console.log(comp1 > comp2);
+console.log(comp1 < comp2);
+console.log(comp1 <= comp2);
+
+// Logical Opearators
+let x =5;
+let y = 6;
+console.log(x<y && x==y);
+console.log(x>y || x==y);
+console.log(!false);
