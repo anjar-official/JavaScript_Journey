2c4192dfa65aa7f37792ee4116b7eeb0743508ae
Anjar Hussain <101003348+anjar-official@users.noreply.github.com>
2023-07-13 05:22:07 -0700

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,20 @@
+// Arrays :- variable that holds multiple values
+
+/*const num = new Array(1,2,3,43,24);
+console.log(num);*/
+
+/*const fruit = ['mango','apple',10,true];  // multiple data types in a single array
+console.log(fruit[1])*/
+
+
+const fruit = ['mango','apple',10,true];  
+fruit[3] = 'grapes'
+console.log(fruit)
+
+/*
+fruit.push('banana');
+fruit.pop();
+fruit.unshift('oranges');
+
+console.log(fruit.indexOf('oranges'));
+*/
