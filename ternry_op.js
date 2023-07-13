9415ecf771351a9ea8163695708c6b44db55cb0c
Anjar Hussain <101003348+anjar-official@users.noreply.github.com>
2023-07-13 06:05:49 -0700

    Add files via upload

---
+++ ternry_op.js
@@ -0,0 +1,20 @@
+/*
+const x =11;
+
+let color = x > 10 ? 'red' : 'blue' ;
+console.log(color);
+*/
+
+let color = 'red' ;
+
+switch(color){
+    case 'red':
+        console.log("Color is red");
+        break;
+    case 'blue':
+        console.log("Color is blue");
+        break;
+    default:
+        console.log("Color is not red or blue");
+        break;
+}
