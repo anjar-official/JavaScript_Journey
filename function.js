6d1949abb5d8ad24a830c2586d7eba5f37a3e2e5
Anjar Hussain <101003348+anjar-official@users.noreply.github.com>
2023-07-13 17:54:23 -0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+/*
+function adNums(num1,num2){
+    return num1+num2;
+}
+console.log(adNums(5,5));
+*/
+
+//Arrow Function
+//Single statement can be written in one line
+const adNums = (num1,num2) => console.log(num1+num2);
+adNums(5,6);
